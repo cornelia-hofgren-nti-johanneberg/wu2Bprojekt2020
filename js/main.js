360b894832c784579cec20b9b9403b92e7030ac9
cornelia-hofgren-nti-johanneberg <60431267+cornelia-hofgren-nti-johanneberg@users.noreply.github.com>
2020-03-11 14:59:53 +0100

    Vecka 11
    Idag har jag skapat js för menyn, jag har inte hunnit stylea den ännu.
    Jag har också fixat ett opacity "lager" över bilderna på huvudsidan för att framhäva texten lite mer.
    Jag har även skapat flik 2 och designat den.

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+function show() {
+    console.log("Testar!")
+    let menu = document.querySelector('.menu');
+    let layer = document.querySelector('.fade-layer')
+  
+    menu.classList.toggle('show');
+    layer.classList.toggle('visible');
+  
+  }
+  
+  let btn = document.querySelector('.menu-button')
+  let layer = document.querySelector('.fade-layer')
+  
+  btn.addEventListener('click',show);
+  layer.addEventListener('click',show);
